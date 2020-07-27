07503d69ea2391efbaceeb0cf05b16598dbfd901
Sílvia Baptista <silviabaptista18@gmail.com>
2020-07-27 18:02:46 +0100

    routes to projects

--- src/routes.js
+++ src/routes.js
@@ -7,106 +7,116 @@ Vue.use(VueRouter)
 var routes = []
 
 function lazyLoad(view) {
-  return () => import(`@/components/${view}.vue`)
+    return () => import(`@/components/${view}.vue`)
 }
 
-function FadeIn () {
-  var allMods = $(".movement, .cover");
-  var lateralMods = $(".lateral, .lateralR");
-  var svg = $("#svg");
+function FadeIn() {
+    var allMods = $(".movement, .cover");
+    var lateralMods = $(".lateral, .lateralR");
+    var svg = $("#svg");
 
-  (function ($) {
-      $.fn.visible = function (partial, adjust) {
-          if (adjust === undefined) {
-              adjust = 0;
-          }
-          var elem = $(this),
-              wind = $(window),
-              viewTop = wind.scrollTop(),
-              viewBottom = viewTop + wind.height(),
-              elem_top = elem.offset().top + adjust,
-              elem_bottom = elem_top + elem.height() - adjust,
-              compareTop = partial === true ? elem_bottom : elem_top,
-              compareBottom = partial === true ? elem_top : elem_bottom;
+    (function ($) {
+        $.fn.visible = function (partial, adjust) {
+            if (adjust === undefined) {
+                adjust = 0;
+            }
+            var elem = $(this),
+                wind = $(window),
+                viewTop = wind.scrollTop(),
+                viewBottom = viewTop + wind.height(),
+                elem_top = elem.offset().top + adjust,
+                elem_bottom = elem_top + elem.height() - adjust,
+                compareTop = partial === true ? elem_bottom : elem_top,
+                compareBottom = partial === true ? elem_top : elem_bottom;
 
-          return compareBottom <= viewBottom && compareTop >= viewTop;
-      };
-  })(jQuery);
+            return compareBottom <= viewBottom && compareTop >= viewTop;
+        };
+    })(jQuery);
 
-  var win = $(window);
+    var win = $(window);
 
-  var xmlTeste = true;
+    var xmlTeste = true;
 
-  allMods.each(function (i, el) {
-      var el = $(el);
-      if (el.visible(true)) {
-          el.addClass("placed");
-      }
-  });
-  svg.each(function (i, el) {
-      var el = $(el);
-      if (el.visible(true)) {
-          xmlTeste &&
-              el.append(
-                  "<img src='assets/img/elemento_animated.svg' alt=''></img>"
-              );
-          xmlTeste = false;
-      }
-  });
-  lateralMods.each(function (i, el) {
-      var el = $(el);
-      if (el.visible(true, 100)) {
-          el.addClass("placed");
-      }
-  });
+    allMods.each(function (i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            el.addClass("placed");
+        }
+    });
+    svg.each(function (i, el) {
+        var el = $(el);
+        if (el.visible(true)) {
+            xmlTeste &&
+                el.append(
+                    "<img src='assets/img/elemento_animated.svg' alt=''></img>"
+                );
+            xmlTeste = false;
+        }
+    });
+    lateralMods.each(function (i, el) {
+        var el = $(el);
+        if (el.visible(true, 100)) {
+            el.addClass("placed");
+        }
+    });
 
-  win.scroll(function (event) {
-      allMods.each(function (i, el) {
-          var el = $(el);
-          if (el.visible(true)) {
-              el.addClass("placed");
-          }
-      });
-      lateralMods.each(function (i, el) {
-          var el = $(el);
-          if (el.visible(true, 100)) {
-              el.addClass("placed");
-          }
-      });
-      svg.each(function (i, el) {
-          var el = $(el);
-          if (el.visible(true)) {
-              xmlTeste &&
-                  el.append(
-                      "<img src='assets/img/elemento_animated.svg' alt=''></img>"
-                  );
-              xmlTeste = false;
-          }
-      });
-  });
+    win.scroll(function (event) {
+        allMods.each(function (i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                el.addClass("placed");
+            }
+        });
+        lateralMods.each(function (i, el) {
+            var el = $(el);
+            if (el.visible(true, 100)) {
+                el.addClass("placed");
+            }
+        });
+        svg.each(function (i, el) {
+            var el = $(el);
+            if (el.visible(true)) {
+                xmlTeste &&
+                    el.append(
+                        "<img src='assets/img/elemento_animated.svg' alt=''></img>"
+                    );
+                xmlTeste = false;
+            }
+        });
+    });
 }
 
 routes = [
-  {
-    path: '*',
-    //component: NotFoundComponent
-  },
-  {
-    path: '/',
-    name: 'index',
-    component: lazyLoad('index')
-  }
+    {
+        path: '*',
+        //component: NotFoundComponent
+    },
+    {
+        path: '/',
+        name: 'index',
+        component: lazyLoad('index')
+    },
+    {
+        path: '/project/:name',
+        name: 'project',
+        component: lazyLoad('includes/project')
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  routes
+    mode: 'history',
+    routes,
+    scrollBehavior: function (to) {
+        if (to.hash) {
+            return $('html,body').stop().animate({ scrollTop: $(to.hash).offset().top - $(to.hash).height() }, 1000)
+        }
+    }
 })
 
 router.afterEach((to, from, next) => {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0
-  FadeIn();
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0
+    FadeIn();
 })
 
 export default router;
